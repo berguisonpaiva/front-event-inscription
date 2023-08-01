01212082ed6a09a3e5fee7d9e412fb829f248e3d
berguisonpaiva <pberguison@gmail.com>
2023-07-31 21:56:45 -0300

    Header

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,13 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom"
 import { Home } from "./pages/Home"
 import { Inscritions } from "./pages/Incriptions"
+import { Header } from "./components/Header"
 
 
 function App() {
   return (
     <div>
-    
+    <Header />
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Home />} />         
